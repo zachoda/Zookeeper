0be0a3e1ef827251a6963190bf4fa306f723ca3a
Zach Oda <zjodachow@gmail.com>
2022-08-09 11:46:09 -0400

    updated port

--- server.js
+++ server.js
@@ -50,5 +50,5 @@ app.get("/api/animals", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}`);
-});
+    console.log(`API server now on port ${PORT}!`);
+  });
