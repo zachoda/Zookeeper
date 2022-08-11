fb098f208cb94ec1bbb7761423890678bce9616c
Zach Oda <zjodachow@gmail.com>
2022-08-11 17:08:22 -0400

    finalized zookeeper

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
-const { animals } = require('./data/animals');
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/apiRoutes");
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+app.use(express.static('public'));
 
-
-
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
